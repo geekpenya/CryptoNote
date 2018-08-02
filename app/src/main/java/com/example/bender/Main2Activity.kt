1e2c1e803fb5f7d8cec3cdcb5ede2396a9472017
geekpenya <geekpenya@users.noreply.github.com>
2018-08-02 18:20:37 +0600

    +Контекстное меню для списка заметок: Удалить заметку

--- app/src/main/java/com/example/bender/Main2Activity.kt
+++ app/src/main/java/com/example/bender/Main2Activity.kt
@@ -3,22 +3,30 @@ package com.example.bender
 import android.content.Context
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteDatabase.openOrCreateDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
+import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.example.bender.NewNoteActivity.Companion.getidfromBD
+import com.example.bender.R.id.del_note
+import com.example.bender.R.id.re_note
 import kotlinx.android.synthetic.main.activity_main2.*
+
+
+
 
 
 class Main2Activity : AppCompatActivity() {
     lateinit var BD: SQLiteDatabase
     val BD_NAME = "NoteBD"
+    lateinit var adapter: NoteViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +40,19 @@ class Main2Activity : AppCompatActivity() {
         loadNoteTitle()
     }
 
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
 
+        when(item!!.itemId) {
+            R.id.del_note -> {
+                adapter.deleteNote()
+                loadNoteTitle()
+            }
+            R.id.re_note -> Toast.makeText(this, "Soon", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(this, "Нет условия", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
+    }
 
     fun butplus(view: View) {
         val newNote = Intent(this, NewNoteActivity::class.java)
@@ -74,8 +94,15 @@ class Main2Activity : AppCompatActivity() {
         BD = openOrCreateDatabase(BD_NAME, Context.MODE_PRIVATE, null)
 
         NoteView.layoutManager = LinearLayoutManager(this)
-        NoteView.adapter = NoteViewAdapter(getTitleNote(), this)
+        adapter = NoteViewAdapter(getTitleNote(), this, BD)
+        NoteView.adapter = adapter
+        registerForContextMenu(NoteView)
     }
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/bender/NoteViewAdapter.kt
+++ app/src/main/java/com/example/bender/NoteViewAdapter.kt
@@ -4,20 +4,22 @@ import android.app.Activity
 import android.app.Person
 import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.nfc.Tag
 import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.view.menu.ActionMenuItemView
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.*
-import kotlin.coroutines.experimental.coroutineContext
 
-class NoteViewAdapter (var titlebox: ArrayList<String>, var context: Context): RecyclerView.Adapter<NoteViewAdapter.ViewHolder>() {
+@Suppress("DEPRECATION")
+class NoteViewAdapter (var titlebox: ArrayList<String>, var context: Context, var BD: SQLiteDatabase): RecyclerView.Adapter<NoteViewAdapter.ViewHolder>() {
 
     override fun getItemCount() = titlebox.size
+
+    private var seletedItem: Int = -1
+
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
 
@@ -34,33 +36,49 @@ class NoteViewAdapter (var titlebox: ArrayList<String>, var context: Context): R
             context.startActivity(noteView)
 
         })
+       p0.textView!!.setOnLongClickListener(View.OnLongClickListener {
+            seletedItem = p1
+           false
+
+       })
         p0?.textView?.text = titlebox[p1]
+    }
 
+    fun deleteNote() {
+        var query = "SELECT photo FROM Note WHERE title = ?;"
+        var cursor = BD.rawQuery(query, arrayOf(titlebox[seletedItem]))
+        cursor.moveToFirst()
+        val photoid = cursor.getInt(cursor.getColumnIndex("photo"))
+        cursor.close()
+        BD.delete("Photo", "Note = ?", arrayOf(photoid.toString()))
+        BD.delete("Note", "Title = ?", arrayOf(titlebox[seletedItem]))
+    }
 
+    fun rewriteNote() {
 
     }
 
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
+
+
+
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnCreateContextMenuListener {
       var textView: TextView? = null
       init {
-         textView = itemView?.findViewById(R.id.text_list_item)
+          textView = itemView?.findViewById(R.id.text_list_item)
+          itemView.setOnCreateContextMenuListener(this)
       }
 
+        override fun onCreateContextMenu(p0: ContextMenu?, p1: View?, p2: ContextMenu.ContextMenuInfo?) {
+
+            p0!!.add(Menu.NONE, R.id.re_note, Menu.NONE, "Редактировать")
+            p0.add(Menu.NONE, R.id.del_note, Menu.NONE, "Удалить")
+
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-  }
+    }
 
 }
 
